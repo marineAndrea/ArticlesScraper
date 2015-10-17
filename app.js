88a0282ba980085d701dccad4f40792b919d3bc8
Marine Guillanton <marine.guillanton@gmail.com>
2015-10-17 13:32:07 -0700

    parses article at given url by extracting all the paragraphs that do not have a  classname

--- app.js
+++ app.js
@@ -6,18 +6,33 @@ angular.module('app', [])
       console.log('url', $scope.article.url);
       $http({
         method: 'GET',
-        url: $scope.article.url
+        url: 'http://www.thedailybeast.com/articles/2014/08/21/swat-lobby-takes-a-shot-at-congress.html'
       })
       .then(function (resp) {
-        $scope.article.text = resp.data;
+        $scope.article.sections = $scope.parseArticle(resp.data);
       });
     };
 
     $scope.saveArticle = function() {
       console.log('savearticle called');
-      var text = $scope.article.text;
+      var text = "";
+      for (var i = 0; i < $scope.article.sections.length; i++) {
+        text += $scope.article.sections[i];
+      }
       var filename = "article";
       var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
       saveAs(blob, filename + ".txt");
     };
+
+    $scope.parseArticle = function(html) {
+      var $output = $(html).find('p');
+      var parag = [];
+      for (var i = 0; i < $output.length; i++) {
+        if ($output[i].className === '') {
+          parag.push($output[i].outerText);
+        }
+      }
+      return parag;
+    };
+
   }]);
